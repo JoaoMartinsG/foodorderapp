9f8b9d6423a08669a2e7bb461ba0f4d3903335e5
Joao Martins <JoaoMGWork@hotmail.com>
2024-02-07 20:16:51 +0000

    Started app with Meals , MealsSummary, AvailableMeals and header component

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import { Fragment } from 'react'
+
+import mealsImage from '../../assets/meals.jpg'
+import HeaderCartButton from './HeaderCartButton'
+import classes from './Header.module.css'
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Food Order App</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt="A table with a full meal" />
+      </div>
+    </Fragment>
+  )
+}
+
+export default Header
