ca34061532494413f4ca161eb7e8fcb136a540da
Joao Martins <JoaoMGWork@hotmail.com>
2024-02-08 16:43:48 +0000

    added add to cart btn effect , ability to add and remove items from cart

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,34 @@
+import { useRef, useState } from 'react'
+
 import classes from './MealItemForm.module.css'
 import Input from '../../UI/Input'
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true)
+  const amountInputRef = useRef()
+
+  const submitHandler = (event) => {
+    event.preventDefault()
+
+    const enteredAmount = amountInputRef.current.value // value is always a string
+    const enteredAmountNumber = +enteredAmount // adding + converts string to number
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsValid(false)
+      return
+    }
+
+    props.onAddToCart(enteredAmountNumber)
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: 'amount',
@@ -15,7 +39,8 @@ const MealItemForm = (props) => {
           defaultValue: '1',
         }}
       />
-      <button>+ Add</button>
+      <button type="submit">+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1-5)</p>}
     </form>
   )
 }
